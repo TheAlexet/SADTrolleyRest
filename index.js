5820a57bbce61d04b31bb240da6be60dfd2f8719
marcgf5 <45403823+marcgf5@users.noreply.github.com>
2021-10-31 12:18:43 +0100

    Trolley rest for trying

---
+++ index.js
@@ -0,0 +1,5 @@
+var testTrolley = require('./my_modules/shoppingTrolley/testTrolley.js');
+var testTrolleyDB = require('./my_modules/shoppingTrolley/testTrolleyDB.js');
+
+testTrolley.testShoppingTrolley();
+testTrolleyDB.testShoppingTrolley();
